19273bd1533bddafb3e2b7542aa8517a7d8b8309
Rumia <lemmings1010@yahoo.co.kp>
2024-11-14 10:59:53 +0900

    ダメージ床作成

--- Assets/Script/HealthCollectable.cs
+++ Assets/Script/HealthCollectable.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HealthCollectable : MonoBehaviour
@@ -8,6 +9,10 @@ public class HealthCollectable : MonoBehaviour
         //Debug.Log("triggerと接触:"+other);
         RubyController rubyCon=other.GetComponent<RubyController>();
         if(rubyCon!=null){
+            if(rubyCon.health==rubyCon.maxHealth){
+                //Debug.Log(rubyCon.health);
+                return;
+                }
             rubyCon.ChangeHealth(1);
             Destroy(gameObject);
         }
--- Assets/Script/RubyController.cs
+++ Assets/Script/RubyController.cs
@@ -8,6 +8,9 @@ public class RubyController : MonoBehaviour
     public int maxHealth = 5;
     int currentHealth;
     public int health{get{return currentHealth;}}
+    public float timeInvincible=2.0f;
+    bool isInvincible;
+    float invincibleTimer;
     Rigidbody2D rb;
     void Start()
     {
@@ -23,9 +26,21 @@ public class RubyController : MonoBehaviour
         position.y = position.y + speed * vertical * Time.deltaTime;
         transform.position = position;
         rb.MovePosition(position);
+
+        if(isInvincible){
+            invincibleTimer-=Time.deltaTime;
+            if(invincibleTimer<0){
+                isInvincible=false;
+            }
+        }
     }
     public void ChangeHealth(int amount)
     {
+        if(amount<0){
+            if(isInvincible) return;
+            isInvincible=true;
+            invincibleTimer=timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth+"/"+maxHealth);
     }
